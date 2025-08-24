fbc018f5c3113681276bd9dcfdfe0db32e7e1858
Amir <amir.taghizade76@gmail.com>
2025-08-24 13:10:51 +0330

    add loaderData

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Root from "./routes/root.jsx";
-import Categories from "./routes/categories.jsx";
+import Genres from "./routes/genres.jsx";
 import ErroPage from "./routes/error.jsx";
+import MoviebyGenre, { MovieByGenreLoader } from "./routes/movieByGenre.jsx";
+// import { useParams } from "react-router";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
     errorElement: <ErroPage />,
-  },
-  {
-    path: "categories",
-    Component: Categories,
+    children: [
+      {
+        path: "genres",
+        Component: Genres,
+        children: [
+          {
+            path: ":genre",
+            Component: MoviebyGenre,
+            loader: MovieByGenreLoader,
+          },
+        ],
+      },
+    ],
   },
 
   // { path: "*", element: <NotFoundPage /> },
