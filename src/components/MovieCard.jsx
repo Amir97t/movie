632278bcc6b8abb50a53788946160cb40b47b77f
Amir <amir.taghizade76@gmail.com>
2025-08-25 18:56:23 +0330

    add Skeleton to get a loading card before fetching data

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,15 @@
 import { Card } from "flowbite-react";
+import { Link } from "react-router";
 
-export default function MovieCard({ id, title, poster, genres }) {
+export default function MovieCard({ id, title, poster }) {
   return (
-    <Card
-      className="w-[282px] h-[480px]"
-      renderImage={() => <img src={poster} />}
-    >
-      <h5 className="text-2xl font-bold tracking-tight text-black ">{title}</h5>
-    </Card>
+    <Link to={`/movies/${id}`}>
+      <Card
+        className="w-[282px] h-[480px]"
+        renderImage={() => <img className="h-[350px]" src={poster} />}
+      >
+        <h5 className="text-xl font-bold text-black ">{title}</h5>
+      </Card>
+    </Link>
   );
 }
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -2,7 +2,7 @@ import { useLoaderData } from "react-router";
 import { useState, useEffect, useRef } from "react";
 import getAllMovies from "../api/getAllMovies";
 import MovieList from "../components/MovieList";
-import Spinner from "../components/Spinner";
+import MovieCardSkeleton from "../components/MovieCardSkeleton";
 
 export async function HomeLoader() {
   const movies = await getAllMovies(1);
@@ -12,17 +12,27 @@ export async function HomeLoader() {
 export default function Home() {
   const { movies } = useLoaderData();
 
-  const [allMovies, setAllMovies] = useState(movies.data.slice(0, 8));
+  const [allMovies, setAllMovies] = useState([]);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(page < movies.metadata.page_count);
+  const [loading, setLoading] = useState(true);
 
   const loaderRef = useRef(null);
+
+ 
+  useEffect(() => {
+    setAllMovies(movies.data.slice(0, 8));
+    setLoading(false);
+  }, [movies]);
+
 
   useEffect(() => {
     if (page === 1) return;
     const fetchMore = async () => {
+      setLoading(true);
       const next = await getAllMovies(page);
       setAllMovies((prev) => [...prev, ...next.data]);
+      setLoading(false);
       if (page >= next.metadata.page_count) {
         setHasMore(false);
       }
@@ -30,33 +40,48 @@ export default function Home() {
     fetchMore();
   }, [page]);
 
+ 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
-        if (entries[0].isIntersecting && hasMore) {
+        if (entries[0].isIntersecting && hasMore && !loading) {
           setPage((prev) => prev + 1);
         }
       },
       { threshold: 1 }
     );
-
     if (loaderRef.current) {
       observer.observe(loaderRef.current);
     }
-
     return () => {
       if (loaderRef.current) observer.unobserve(loaderRef.current);
     };
-  }, [hasMore]);
+  }, [hasMore, loading]);
 
   return (
     <div className="p-5">
       <h1 className="text-xl font-bold mb-4">Movies</h1>
-      <MovieList movies={allMovies} />
+
+     
+      {loading && allMovies.length === 0 ? (
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {Array.from({ length: 8 }).map((_, idx) => (
+            <MovieCardSkeleton key={idx} />
+          ))}
+        </div>
+      ) : (
+        <MovieList movies={allMovies} />
+      )}
 
       {hasMore && (
-        <div ref={loaderRef} className="text-center p-4 text-gray-500">
-          <Spinner /> Loading more...
+        <div ref={loaderRef} className="flex justify-center mt-4">
+          {loading && allMovies.length > 0 && (
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-full">
+              {Array.from({ length: 4 }).map((_, idx) => (
+                <MovieCardSkeleton key={idx} />
+              ))}
+            </div>
+          )}
         </div>
       )}
     </div>
