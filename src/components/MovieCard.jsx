df064c05d98f7ddbec3c632f4324763c34bd2cc8
Amir <amir.taghizade76@gmail.com>
2025-08-28 11:32:18 +0330

    Add more style to MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,35 @@
 import { Card } from "flowbite-react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
-export default function MovieCard({ id, title, poster }) {
+export default function MovieCard({ id, title, poster, imdb_rating }) {
   return (
-    <Link to={`/movies/${id}`}>
-      <Card
-        className="w-[282px] h-[480px]"
-        renderImage={() => <img className="h-[350px]" src={poster} />}
-      >
-        <h5 className="text-xl font-bold text-black ">{title}</h5>
-      </Card>
-    </Link>
+    <div className="relative w-[282px] h-[480px]">
+      <div className="absolute top-3 left-3 w-14 h-10 rounded 
+      bg-[#000000A6] text-[#FFAD49] flex items-center 
+      justify-center gap-1">
+        <img
+          className="h-4 w-4"
+          src="../src/assets/star.svg"
+          alt="rating-star"
+        />
+        <span className="text-sm font-semibold">{imdb_rating}</span>
+      </div>
+      <Link to={`/movies/${id}`}>
+        <Card
+          className=" bg-[#20283ECC] border-0"
+          renderImage={() => (
+            <img
+              className="h-[380px] w-[266px] rounded object-cover self-center mt-2"
+              src={poster}
+              alt={title}
+            />
+          )}
+        >
+          <h5 className="text-left text-[16px] font-[Poppins] font-semibold text-[#EBEEF5]">
+            {title}
+          </h5>
+        </Card>
+      </Link>
+    </div>
   );
 }
