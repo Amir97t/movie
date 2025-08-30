e4106824b36b5f3e9d72792c2060dc6f5ee5aeff
Amir <amir.taghizade76@gmail.com>
2025-08-30 18:01:24 +0330

    Add hover to navbar

---
+++ src/theme/index.js
@@ -0,0 +1,11 @@
+import { createTheme } from "flowbite-react";
+
+export default createTheme({
+  navbar: {
+    link: {
+      active: {
+        off: "dark:hover:text-[#7B6EF6] ",
+      },
+    },
+  },
+});
