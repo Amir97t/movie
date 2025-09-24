9a6147469d3f97dc27d3e80828c08132cf2d315d
Amir <amir.taghizade76@gmail.com>
2025-09-24 11:37:01 +0330

    Removed "Main" to simplfy App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import Navbar from "./components/NavBar";
-import Main from "./components/Main";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div className="w-[1440px] mx-auto ">
-        <div className="mx-20">
-          <Navbar />
-          <main >
-            <Main />
-          </main>
-          <div >
-            <Outlet />
-          </div>
-        </div>
+    <div className="w-[1440px] mx-auto">
+      <div className="mx-20">
+        <Navbar />
+        <Outlet />
       </div>
-    </>
+    </div>
   );
 }
-
 export default App;
