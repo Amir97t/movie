18de3fa1f7a7fd19b5423ec16a5c776eace92851
Amir <amir.taghizade76@gmail.com>
2025-11-27 11:59:01 +0330

    Add farsi error

--- src/routes/error.jsx
+++ src/routes/error.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router";
 import NavBar from "../components/NavBar";
 import { useRouteError } from "react-router";
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 export default function ErroPage() {
   const error = useRouteError();
-
+  const { t } = useTranslation();
   return (
     <motion.div
       initial={{ opacity: 0, y: 30 }}
@@ -18,14 +19,15 @@ export default function ErroPage() {
       <div className="lg:mx-20 md:mx-15 mx-3">
         <NavBar />
         <div className="items-center text-center mt-15 text-white grid place-items-center gap-4">
-          <img src="src/assets/error.svg" className="w-[400px] h-[320px]" />
-          <h2 className="font-[Poppins] text-5xl">Lost you'r way?</h2>
+          <img src="src/assets/error.svg" className="w-[400px] h-80" />
+          <h2 className="font-[Poppins] text-5xl">
+            {t("errorPage.error_route_title")}
+          </h2>
           {error && (
             <p className="text-[#8E95A9]">
               {error.status}{" "}
               <span className="font-bold">{error.statusText}</span>
-              --- Oops! This is awkward. You are looking for something that
-              doesn't actually exist.
+              {t("errorPage.error_route_message")}
             </p>
           )}
           <p>{error && error.error && error.error.message}</p>
@@ -34,7 +36,7 @@ export default function ErroPage() {
             as={Link}
             to={"/"}
           >
-            Go Home
+            {t("errorPage.error_goBack")}
           </Button>
         </div>
       </div>
