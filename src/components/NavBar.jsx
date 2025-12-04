1498323603b6e25c313604b27c6a750c942d3a5f
Amir <amir.taghizade76@gmail.com>
2025-12-04 13:03:59 +0330

    Add mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,48 +18,52 @@ export default function AppBar() {
       theme={theme.navbar}
       className="bg-[#00000033] font-[Poppins] w-full rounded-lg backdrop-blur-md"
     >
-      <NavbarBrand>
-        <Link to="/">
-          <img
-            src="../src/assets/icon-tab.svg"
-            className="h-6 sm:h-8 md:h-9"
-            alt="Logo"
-          />
-        </Link>
+      <div className="flex items-center">
+        <NavbarBrand as={Link} to="/">
+          <span>
+            <img
+              src="../src/assets/icon-tab.svg"
+              className="h-6 sm:h-8 md:h-9"
+              alt="Logo"
+            />
+          </span>
+        </NavbarBrand>
         <LanguageSwitch />
-      </NavbarBrand>
-      <NavbarToggle className="text-[#7B6EF6] hover:cursor-pointer hover:bg-[#ffffff22]" />
-      <NavbarCollapse className="text-center md:text-left">
-        <NavbarLink
-          className={`text-white hover:cursor-pointer  ${
-            i18n.language === "fa" ? "text-[15px]" : ""
-          }`}
-          as={Link}
-          to="/random"
-        >
-          {t("home.nav_random")}
-        </NavbarLink>
+      </div>
+   
+     <NavbarToggle />
 
-        <NavbarLink
-          className={`text-white hover:cursor-pointer ${
-            i18n.language === "fa" ? "text-[15px]" : ""
-          }`}
-          href="#"
-        >
-          {t("home.nav_middle")}
-        </NavbarLink>
+  <NavbarCollapse className="text-center md:text-left space-y-4 md:space-y-0">
+    <NavbarLink
+      className={`text-white hover:cursor-pointer ${
+        i18n.language === "fa" ? "text-[15px]" : ""
+      }`}
+      as={Link}
+      to="/random"
+    >
+      {t("home.nav_random")}
+    </NavbarLink>
 
-        <NavbarLink
-          className={`flex justify-center md:justify-start text-white gap-2 hover:cursor-pointer ${
-            i18n.language === "fa" ? "text-base" : ""
-          }`}
-          as={Link}
-          to="/"
-        >
-          {t("home.nav_home")}
-          <img src="../src/assets/arrow-right.svg" alt="arrow" />
-        </NavbarLink>
-      </NavbarCollapse>
+    <NavbarLink
+      className={`text-white hover:cursor-pointer ${
+        i18n.language === "fa" ? "text-[15px]" : ""
+      }`}
+      href="#"
+    >
+      {t("home.nav_middle")}
+    </NavbarLink>
+
+    <NavbarLink
+      className={`flex justify-center md:justify-start text-white gap-2 hover:cursor-pointer ${
+        i18n.language === "fa" ? "text-base" : ""
+      }`}
+      as={Link}
+      to="/"
+    >
+      {t("home.nav_home")}
+      <img src="../src/assets/arrow-right.svg" alt="arrow" />
+    </NavbarLink>
+  </NavbarCollapse>
     </Navbar>
   );
 }
